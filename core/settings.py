64f47e70a1c9efeb05e068bee165af3ee8a94cd8
devemmanuel1 <emmanuelezechukwu0@gmail.com>
2024-02-19 15:32:58 +0100

    nothing changed

--- core/settings.py
+++ core/settings.py
@@ -52,15 +52,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# question4:
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-}
-
-# question6:
-REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
